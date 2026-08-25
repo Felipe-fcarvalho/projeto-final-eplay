8e73e2a7f405e73a8b28a477657df5617f66031d
felipe-fcarvalho <felipefcarvalho7@gmail.com>
2026-08-25 20:45:08 -0300

    feat: parte 1 finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { BrowserRouter } from 'react-router-dom'
+
 import { GlobalCss } from './styles'
 import Header from './components/Header'
 
+import Rotas from './routes'
+import Footer from './components/Footer'
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalCss />
-      <Header />
-      <div>ola mundo</div>
-    </>
+      <div className="container">
+        <Header />
+      </div>
+      <Rotas />
+      <Footer />
+    </BrowserRouter>
   )
 }
 
--- src/styles.ts
+++ src/styles.ts
@@ -4,19 +4,28 @@ export const cores = {
   branca: '#EEE',
   preta: '#111',
   cinza: '#333',
-  verde: '#10AC84'
+  verde: '#10AC84',
+  cinzaClaro: '#A3A3A3'
 }
 
 export const GlobalCss = createGlobalStyle`
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: Roboto, sans-serif;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style: none;
   }
 
-body {
-  background-color: ${cores.preta}/
-  color: ${cores.branca}
-}
+  body {
+    background-color: ${cores.preta};
+    color: ${cores.branca};
+    padding-top: 40px;
+  }
+
+  .container {
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto;
+  }
 `
